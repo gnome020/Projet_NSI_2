335dd508115d6f4786f19ad3d4c3cb8cac6083be
Ash <ash.mnnln@gmail.com>
2024-04-30 09:38:27 +0200

    menu qui fonctionne

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from menu import *
+from menu import init_buttons
 from paramètre import *
 from niveau_1 import Niveau_1
 from niveau_2 import Niveau_2
@@ -16,9 +16,8 @@ class Game:
         self.niveau_en_cours = 0
         self.niveau = [Niveau_1(self), Niveau_2(self)]
 
-        # Création des boutons
-        self.start_button = Button("../graphics/menu/start_button.png", x_start_button, y_start_button)
-        self.exit_button = Button("../graphics/menu/exit_button.png", x_exit_button, y_exit_button)
+        # Initialisation des boutons
+        self.resume_button, self.exit_button = init_buttons()
 
         # Initialisation de l'état du menu
         self.show_menu = False
@@ -29,14 +28,14 @@ class Game:
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.show_menu = not self.show_menu  # Basculer l'état du menu
-                    elif event.key == pygame.K_q:  # Pour quitter le jeu avec la touche Q
+                    elif event.key == pygame.K_p:  # Pour quitter le jeu avec la touche Q
                         pygame.quit()
                         sys.exit()
                 elif event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN and self.show_menu:
-                    if self.start_button.rect.collidepoint(pygame.mouse.get_pos()):
+                    if self.resume_button.rect.collidepoint(pygame.mouse.get_pos()):
                         self.show_menu = False
                     elif self.exit_button.rect.collidepoint(pygame.mouse.get_pos()):
                         pygame.quit()
@@ -46,7 +45,7 @@ class Game:
 
             if self.show_menu:
                 # Dessiner le menu ici
-                self.start_button.draw(self.ecran)
+                self.resume_button.draw(self.ecran)
                 self.exit_button.draw(self.ecran)
             else:
                 self.niveau[self.niveau_en_cours].run()
--- code/menu.py
+++ code/menu.py
@@ -1,26 +1,29 @@
 import pygame
 
-pygame.init()
+# Créer une fonction d'initialisation des boutons
+def init_buttons():
+    pygame.init()  # Initialisation de Pygame ici
 
-# Créer les boutons
-class Button:
-    def __init__(self, image_path, x, y):
-        self.image = pygame.image.load(image_path).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+    # Créer les boutons
+    class Button:
+        def __init__(self, image_path, x, y):
+            self.image = pygame.image.load(image_path).convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.topleft = (x, y)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        def draw(self, surface):
+            surface.blit(self.image, self.rect)
 
-x_start_button = 100  # Remplacez par la position X désirée
-y_start_button = 100  # Remplacez par la position Y désirée
-x_exit_button = 100   # Remplacez par la position X désirée
-y_exit_button = 200   # Remplacez par la position Y désirée
+    x_resume_button = 100  # Remplacez par la position X désirée
+    y_resume_button = 100  # Remplacez par la position Y désirée
+    x_exit_button = 100   # Remplacez par la position X désirée
+    y_exit_button = 200   # Remplacez par la position Y désirée
 
-# Chemins d'accès aux images
-exit_img_path = "Projet_NSI_2/graphics/menu/exit_button.png"
-start_img_path = "Projet_NSI_2/graphics/menu/start_button.png"
+    # Chemins d'accès aux images
+    exit_img_path = "../graphics/menu/exit_button.png"
+    resume_img_path = "../graphics/menu/start_button.png"
 
-# Créer les instances de boutons
-start_button = Button(start_img_path, x_start_button, y_start_button)
-exit_button = Button(exit_img_path, x_exit_button, y_exit_button)
+    # Créer et retourner les instances de boutons
+    resume_button = Button(resume_img_path, x_resume_button, y_resume_button)
+    exit_button = Button(exit_img_path, x_exit_button, y_exit_button)
+    return resume_button, exit_button
